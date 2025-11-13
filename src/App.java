5a47a785660177454e39fdd39c3073272a001d26
tommiHAMK <tommi.saksa@hamk.fi>
2025-11-13 19:36:41 +0200

    Ylikirjoitus, override

--- src/App.java
+++ src/App.java
@@ -1,8 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Koira koira1 = new Koira();
+        Lintu lintu1 = new Lintu();
         koira1.liiku();
         koira1.hauku();
+        lintu1.liiku();
+        lintu1.tulostaTiedot();
     }
 } // Appin lopetus
 
@@ -12,6 +15,10 @@ class Elain {
     public void liiku() {
         System.out.println("Eläin liikkuu");
     }
+
+    public void tulostaTiedot() {
+        System.out.println("Paino: " + paino);
+    }
 }
 
 class Koira extends Elain {
@@ -20,6 +27,11 @@ class Koira extends Elain {
     public void hauku() {
         System.out.println("Koira haukkuu");
     }
+
+    @Override
+    public void liiku() {
+        System.out.println("Koira juoksee");
+    }
 }
 
 class Lintu extends Elain {
@@ -27,5 +39,10 @@ class Lintu extends Elain {
 
     public void haudo() {
         System.out.println("Lintu hautoo");
+    }
+
+    @Override
+    public void liiku() {
+        System.out.println("Lintu lentää");
     }
 }
