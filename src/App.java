4a3ad8ebef285e624beec84d882078b4820dbe2c
tommiHAMK <tommi.saksa@hamk.fi>
2025-11-13 19:26:51 +0200

    Koira-luokan eka versio

--- src/App.java
+++ src/App.java
@@ -1,5 +1,23 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        Koira koira1 = new Koira();
+        koira1.liiku();
+        koira1.hauku();
+    }
+} // Appin lopetus
 
+class Elain {
+    public double paino;
+
+    public void liiku() {
+        System.out.println("Eläin liikkuu");
+    }
+}
+
+class Koira extends Elain {
+    String rotu;
+
+    public void hauku() {
+        System.out.println("Koira haukkuu");
     }
 }
